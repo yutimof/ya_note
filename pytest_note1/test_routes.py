f83fe9714015e6d42aa64778373aa6c7776d1276
yutimof <yutimof@gmail.com>
2024-09-08 22:50:24 +0200

    измения следующие

---
+++ pytest_note1/test_routes.py
@@ -0,0 +1,18 @@
+# test_routes.py
+
+from http import HTTPStatus
+
+from django.urls import reverse
+
+import pytest
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
