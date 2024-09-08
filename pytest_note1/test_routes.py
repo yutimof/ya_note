63405f90c8102302f8e21ad632072150a5e9cd26
yutimof <yutimof@gmail.com>
2024-09-09 00:24:09 +0200

    changes 3

--- pytest_note1/test_routes.py
+++ pytest_note1/test_routes.py
@@ -16,3 +16,12 @@ def test_pages_availability_for_anonymous_user(client, name):
     url = reverse(name)  # Получаем ссылку на нужный адрес.
     response = client.get(url)  # Выполняем запрос.
     assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(admin_client, name):
+    url = reverse(name)
+    response = admin_client.get(url)
+    assert response.status_code == HTTPStatus.OK 
